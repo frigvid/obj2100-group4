5c8cbc51f73f2fc8431b1cd32550ccca60139a3d
frigvid <67565373+frigvid@users.noreply.github.com>
2024-05-14 19:22:24 +0200

    feat: Ensure database creation if missing.

--- server/src/main/java/usn/obj2100/Main.java
+++ server/src/main/java/usn/obj2100/Main.java
@@ -1,9 +1,17 @@
 package usn.obj2100;
 
+import java.sql.Connection;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
+		/* This is pretty temporary. But this ensures the database will be created if missing.
+		 * It'll do the same wherever else something tries to get a connection, but at least this
+		 * way it ensures it'll be there if the code for some reason assumes it'll already be in place.
+		 */
+		Connection dcm = DatabaseConnectionManager.getInstance().getConnection();
+		
 		Server server = new Server();
 		server.start();
 	}
