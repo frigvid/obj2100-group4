705267ccc7fbbb23bb7743ec0e9b635f81ad3a36
frigvid <67565373+frigvid@users.noreply.github.com>
2024-05-15 10:00:50 +0200

    feat: Make sure all modules have a module-info file, since IntelliJ has apparently taken a vacation.

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module usn.obj2100 {
-    requires javafx.controls;
-    requires javafx.fxml;
-	 requires java.sql;
-    requires org.controlsfx.controls;
-	 requires shared;
-
-    opens usn.obj2100 to javafx.fxml;
-    exports usn.obj2100;
+module client {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires org.controlsfx.controls;
+	requires shared;
+	
+	opens usn.obj2100.client to javafx.fxml;
+	exports usn.obj2100.client;
 }
