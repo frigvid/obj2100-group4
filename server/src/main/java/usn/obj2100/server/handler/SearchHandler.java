9a50d70450ab209b78349d57e013ec45834adaab
frigvid <67565373+frigvid@users.noreply.github.com>
2024-05-15 19:29:10 +0200

    feat: WIP Search handler class.

---
+++ server/src/main/java/usn/obj2100/server/handler/SearchHandler.java
@@ -0,0 +1,49 @@
+package usn.obj2100.server.handler;
+
+import usn.obj2100.server.controller.*;
+import usn.obj2100.shared.model.Search;
+
+import java.util.List;
+
+public class SearchHandler
+{
+	private final InventarController inventarController;
+	private final PlasseringController plasseringController;
+	private final KategoriController kategoriController;
+	private final KategoriTypeController kategoriTypeController;
+	private final KassertController kassertController;
+	private final KassertTypeController kassertTypeController;
+	
+	public SearchHandler()
+	{
+		inventarController = new InventarController();
+		plasseringController = new PlasseringController();
+		kategoriController = new KategoriController();
+		kategoriTypeController = new KategoriTypeController();
+		kassertController = new KassertController();
+		kassertTypeController = new KassertTypeController();
+	}
+	
+	//private SearchHandler
+	//(
+	//	InventarController inventarController,
+	//	PlasseringController plasseringController,
+	//	KategoriController kategoriController,
+	//	KategoriTypeController kategoriTypeController,
+	//	KassertController kassertController,
+	//	KassertTypeController kassertTypeController
+	//)
+	//{
+	//	this.inventarController = inventarController;
+	//	this.plasseringController = plasseringController;
+	//	this.kategoriController = kategoriController;
+	//	this.kategoriTypeController = kategoriTypeController;
+	//	this.kassertController = kassertController;
+	//	this.kassertTypeController = kassertTypeController;
+	//}
+	
+	//public List<Object> handleSearch(Search search)
+	//{
+	//
+	//}
+}
